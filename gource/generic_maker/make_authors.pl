0bc866bfe80b13c7222a6297c8fa735e14d5df35
Apocalypse <perl@0ne.us>
2010-03-27 11:57:25 -0700

    first stab at generic authors parsing

---
+++ gource/generic_maker/make_authors.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+use strict; use warnings;
+use Encode;
+
+# chdir to the git source path
+if ( defined $ARGV[0] and -d $ARGV[0] ) {
+	chdir( $ARGV[0] ) or die "Unable to chdir to $ARGV[0]: $!";
+} else {
+	die "Please supply a path to the git repository: '$0 Moose/\n";
+}
+
+# Get the authors
+my %authors;
+{
+	my @authors = `git log --pretty=format:user:%aN --reverse --raw --encoding=UTF-8 --no-renames | grep "user:*" | cut -c 6-100 | sort -u`;
+	foreach my $a ( @authors ) {
+		chomp $a;
+		my $len = length ( decode( "utf8", $a ) );
+		$authors{ $a } = $len;
+	}
+}
+
+# Get the "longest" author name, heh
+my $longest = 0;
+foreach my $a ( keys %authors ) {
+
+	if ( $authors{$a} > $longest ) {
+		$longest = $authors{$a};
+	}
+}
+
+# Make sure the longest ends perfectly aligned with a tab boundary
+if ( $longest % 8 != 0 ) {
+	$longest += abs( $longest - ( 8 * ( int($longest / 8) + 1 ) ) );
+}
+
+# Make the output
+my $output = "my \%AUTHORS = (\n";
+foreach my $a ( sort { lc($a) cmp lc($b) } keys %authors ) {
+	# Figure out how many tabs we need :)
+	my $charsdiff = ( $longest - $authors{ $a } ) - 2; # subtract two for the "$a"
+
+	my $tabs;
+	if ( $charsdiff % 8 == 0 ) {
+		$tabs = int( $charsdiff / 8 );
+	} else {
+		$tabs = int( $charsdiff / 8 ) + 1; # +1 so it lines up properly
+	}
+
+
+
+	$output .= "\t\"$a\"" . ( "\t" x $tabs ) . "=> 'PAUSEID',\n";
+}
+
+$output .= ");\n";
+print $output;
